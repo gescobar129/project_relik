5ea689bf8901aa202821d5090070c3fdff5b0da6
Justin Hardin <jhard9000@gmail.com>
2022-11-19 22:58:48 -0800

    integrating crypto

---
+++ Project Relik/Assets/Scripts/Network/GameController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net.Http;
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.OpenURL("https://sparkling-griffin-09b3bd.netlify.app/wallet-login");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
